9a617ad9191cd78c2df8290b0e9954348da208e9
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2024-06-23 04:42:32 +0000

    Bootstrap Styles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react'
 import './App.css';
 import './styles/app-style.css'
 import styles from './app.module.css'
+import { Button } from 'react-bootstrap';
 
 export default function App() {
   return (
-    <div className='App'>
+    <div >
+      <div className='App'>
       <h1>There are three main Styles</h1>
       <h1 style={{backgroundColor:'red',color:'teal'}} >Style 1</h1>
       <h1 className='change-bg' >Style 2</h1>
       <h1 className={styles.success} >Style 3</h1>
+      <Button title='Admin' variant="primary" size="sm">
+          Small button
+        </Button>
+      </div>
+      
     </div>
   )
 }
