32eecb63f0c3d26e9db7661e04d69cc6553616e4
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2024-06-23 14:48:35 +0000

    Base Code

--- src/components/Greet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Greet({makeBhakti}) {
-  return (
-    <div>
-      <button style={{width:100,height:30}} onClick={()=>makeBhakti('Har Har Mahadev')} >🙏</button>
-    </div>
-  )
-}
--- src/logics/ForwardRef.js
+++
@@ -1,9 +0,0 @@
-import React, { forwardRef } from 'react'
-
-function ForwardRef(props,rcvdRef) {
-  return (
-    <div ref={rcvdRef} >ForwardRef</div>
-  )
-}
-
-export default forwardRef(ForwardRef)
