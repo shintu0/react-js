cd955795d29d2a58d7d94c6203fb7b8d5bd7bfc1
Sintu Tiwari <125259725+shintu0@users.noreply.github.com>
2025-03-05 17:48:19 +0000

    Basic Todo With Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css'
+import { useState } from 'react'
+import { addTodo, deleteTodo } from './redux/reducer';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todo, setTodo] = useState("");
+
+  const todos = useSelector((state) => state.todos?.value)
+  const dispatch = useDispatch()
+
+  const handleDeleteTodo = (index) => {
+    dispatch(deleteTodo({id:index}));
+  }
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    console.log(todos)
+    dispatch(addTodo({id:todos.length,title:todo}));
+    setTodo("");
+
+  }
 
   return (
     <>
+      <h1>Todo App</h1>
+      <p>My first React App</p>
+      <form  style={{display: "flex", justifyContent:'space-between',flex:'row',}} onSubmit={handleAddTodo} action="" >
+        <input type="text" placeholder="Add todo" name={"todo"} value={todo} onChange={e => setTodo(e.target.value)} />
+        <button type="submit" onSubmit={handleAddTodo} >Add</button>
+      </form>
+      <br/>
+      <h1>TODOS</h1>
+      <hr />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {todos && todos.map((todo, index) => (
+          <div style={{display: "flex", justifyContent:'space-between',flex:'row',}} key={todo.id}>
+            <p>{index+1}. {todo.title}</p>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </div>
+        ))
+
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
